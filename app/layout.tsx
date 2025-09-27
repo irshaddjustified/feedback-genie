9ebca5067164bb10f2fc1c6f6a02eaf28eb45201
hiradev <152847740+hirushaC@users.noreply.github.com>
2025-09-27 09:31:51 +0530

    fix theme issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
       <body className="font-sans antialiased">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Suspense>
